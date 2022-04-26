fc534c541e83a691854da036bab9f1bb0812396a
Edwin Joassart <edwin@blck.coffee>
2022-04-26 13:48:31 +0200

    fix typo

--- watchDots.sh
+++ watchDots.sh
@@ -5,7 +5,7 @@ inotifywait -m /files/dots/ -e create -e moved_to |
   while read dir action file; do
     echo "The file '$file' appeared in directory '$dir' via '$action'"
     # TODO: use a ENV var to choose the output format between pdf and png
-    if [[ -z "$DOT_PROCESSING_PNG"]]; then
+    if [[ -z "$DOT_PROCESSING_PNG"]]; then
       dot -Tpdf /files/dots/$file > /files/$file.pdf;
     else 
       dot -Tpng /files/dots/$file > /files/$file.png;
