5fa8575fee5151d21bf6f7177001fbe2fdd33e41
Edwin Joassart <edwin@blck.coffee>
2022-04-28 15:56:17 +0200

    fix typo

--- watchDots.sh
+++ watchDots.sh
@@ -3,9 +3,9 @@
 inotifywait -m /files/dots/ -e create -e moved_to |
   while read dir action file; do
     echo "The file '$file' appeared in directory '$dir' via '$action'"
-    if [[ -z "$DOT_PROCESSING_PNG"]]; then
-      dot -Tpdf /files/dots/$file > /files/$file.pdf;
+    if [[ -z "$DOT_PROCESSING_PNG" ]]; then
+      dot -Tpdf /files/dots/$file > /files/$file.pdf
     else 
-      dot -Tpng /files/dots/$file > /files/$file.png;
+      dot -Tpng /files/dots/$file > /files/$file.png
     fi
   done
